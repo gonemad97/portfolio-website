4e5677b6fb4f899e10261c0b05439bcac8c3f9b9
Madhuri Palanivelu <palsmadhu18@gmail.com>
2023-02-04 22:13:50 -0700

    added required components with respective css files

--- react-portfolio-website/src/App.jsx
+++ react-portfolio-website/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Contact from './components/contact/Contact'
+import Landing from './components/landing/Landing'
+import Work from './components/work/Work'
+import About from './components/about/About'
+import Footer from './components/footer/Footer'
+
 
 const App = () => {
-  return (
-    <div>App</div>
+  return ( 
+  <>
+    <Navbar />
+    <Landing />
+    <Work />
+    <About />
+    <Contact />
+    <Footer />
+  </>
   )
 }
 
