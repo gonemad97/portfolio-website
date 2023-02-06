26dc40591af3e657d6858d4fafca899a8703d08e
Madhuri Palanivelu <palsmadhu18@gmail.com>
2023-02-05 17:17:40 -0700

    added call-to-action buttons for landing page

--- react-portfolio-website/src/components/landing/Landing.jsx
+++ react-portfolio-website/src/components/landing/Landing.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './landing.css'
+import CTA from './CTA'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Madhuri Palanivelu</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
